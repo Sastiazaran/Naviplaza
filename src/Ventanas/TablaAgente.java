bf0825917f63daa047cf160f84f12cf4d4a42935
888Lucy888 <chuggaaconroy888@gmail.com>
2023-11-23 10:38:46 -0600

    Tablas Funcionales :)

--- src/Ventanas/TablaAgente.java
+++ src/Ventanas/TablaAgente.java
@@ -6,12 +6,12 @@ import Agentes.Agentes;
 import Agentes.Estados;
 
 public class TablaAgente extends JFrame {
-    private static DefaultTableModel dtm;
+    private DefaultTableModel dtm;
     private JScrollPane scrollPane;
     private JTable table;
-    private static Agentes[] agentes;
-    private static int totalAg;
-    private static int type;
+    private Agentes[] agentes;
+    private int totalAg;
+    private int type;
 
     public TablaAgente(Agentes[] a, int n, String[] columnNames, int t) {
         agentes = a;
